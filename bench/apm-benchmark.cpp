4b05857996a30b027ae00e11648820b76f4567a1
Alexandre Jesus <adbjesus@gmail.com>
2022-08-03 13:14:52 +0100

    Fix warnings in apm-benchmark

--- bench/apm-benchmark.cpp
+++ bench/apm-benchmark.cpp
@@ -5,12 +5,12 @@
 #include <ranges>
 
 static void BM_greedy_model(benchmark::State& state) {
-  int n = state.range(0);
-  int l = state.range(1);
+  long n = state.range(0);
+  long l = state.range(1);
   double d = 2.0 / static_cast<double>(state.range(2));
   auto r = apm::point{0.0, 0.0};
   auto segments = apm::piecewise_segments(l, d);
-  for (auto _ : state) {
+  for (auto _s : state) {
     auto model = apm::greedy_model(segments, r);
     for (auto [_, hv] : model | std::ranges::views::take(n)) {
       benchmark::DoNotOptimize(hv);
@@ -20,12 +20,12 @@ static void BM_greedy_model(benchmark::State& state) {
 BENCHMARK(BM_greedy_model)->ArgsProduct({{10, 100, 1000, 10000, 100000}, {10, 100, 1000}, {4, 1}});
 
 static void BM_exact_model(benchmark::State& state) {
-  int n = state.range(0);
-  int l = state.range(1);
+  long n = state.range(0);
+  long l = state.range(1);
   double d = 2.0 / static_cast<double>(state.range(2));
   auto r = apm::point{0.0, 0.0};
   auto segments = apm::piecewise_segments(l, d);
-  for (auto _ : state) {
+  for (auto _s : state) {
     auto model = apm::exact_model(segments, r);
     for (auto hv : model | std::ranges::views::take(n)) {
       benchmark::DoNotOptimize(hv);
